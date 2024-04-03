409fb260b96f428946a04134d339c8b1674cd69f
kabandapatrick2580 <sendsome2019@gmail.com>
2024-04-03 23:05:54 +0200

    updates

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache inherits from BaseCaching and is a caching system. """
+
+    def __init__(self):
+        """ Initialize FIFOCache """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # FIFO: remove the first item added
+                key_discarded_key = next(iter(self.cache_data))
+                del self.cache_data[key_discarded_key]
+                print("DISCARD:", key_discarded_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
+    
